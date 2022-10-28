ec4d50ee409984d33d2a726277c9d539e69fd521
joelquinteros99 <joelquinteros3@gmail.com>
2022-10-28 19:08:37 -0300

    joel

--- oasis/src/components/Footer/Footer.js
+++ oasis/src/components/Footer/Footer.js
@@ -1,14 +1,36 @@
 import React from 'react'
 import './Footer.css'
-import blancoOasis from '../../media/blancoOasis.png'
+// import blancoOasis from '../../media/blancoOasis.png'
 
 function Footer() {
   return (
     <>
-      <div className='footerContainer'>
-        <img className='logo' src={blancoOasis} alt="logo" />
-      </div>
-      
+      <footer className='footerContainer'>
+        <div className="footerInfo">
+          <h1>OASIS</h1>
+          <p>Ciudad</p>
+        </div>
+        <div className="footerContact">
+          <h3>Contactos</h3>
+          <p>numero mail paginaWeb direccion</p>
+        </div>
+        <div className='footerMedia'>
+          <div className=''>
+
+          </div>
+          <div className='footerLinks'>
+            <a href="https://www.google.com/" target="_blank" rel='noreferrer'>
+              <i>Instagram</i>
+            </a>
+            <a href="https://www.google.com/" target="_blank" rel='noreferrer'>
+              <i>Facebook</i>
+            </a>
+            <a href="https://www.google.com/" target="_blank" rel='noreferrer'>
+              <i>Twitter</i>
+            </a>
+          </div>
+        </div>
+      </footer>
     </>
   )
 }
--- oasis/src/components/LandingPage/LandingPage.js
+++ oasis/src/components/LandingPage/LandingPage.js
@@ -3,15 +3,17 @@ import './LandingPage.css'
 import homeVideo from '../../media/homeVideo.mp4'
 import Footer from '../Footer/Footer'
 import {Link} from 'react-router-dom'
+import negroOasis from '../../media/negroOasis.png'
 
 function LandingPage() {
   return (
     <>
       <div className='homeContainer'>
         <video className='video' src={homeVideo} autoPlay loop muted />
-        <h1>OASIS</h1>
-        <p>ALQUILER DE AUTOS EN MENDOZA</p>
-        <div>
+        <div className='contenedorImagen'>
+          <img src={negroOasis} alt="imagen" />
+        </div>
+        <div className='contenedorBotones'>
           <Link to={'/home'}>
             <button type="button" class="btn btn-dark">INICIO</button>
           </Link>
